eab3f07835c599762641dd11c346542f45df698a
Teodora Georgescu <teo@Teodoras-MacBook-Pro.local>
2019-08-11 15:56:13 +0300

    Fixed array initialisation and insertion

--- Arrays/Implementation/ImplementationTest.py
+++ Arrays/Implementation/ImplementationTest.py
@@ -4,12 +4,15 @@ import Arrays.Implementation.Array as myArr
 
 if __name__ == "__main__":
 
-    myArray = myArr.Array(4)
-    myArray.assign(0,0)
-    myArray.assign(1,1)
-    myArray.assign(2,2)
-    myArray.assign(3,3)
+    # Initialise array
+    myArray = myArr.Array([1,2,3,4])
+    myArray.show()
 
+    # Assign a value to an index
+    myArray.assign(92, 0)
+    myArray.show()
+
+    # Observe what happens to the array when we insert a new element
     myArray.insert(452, 2)
 
 
